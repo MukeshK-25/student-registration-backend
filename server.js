e11eb3adfbe41c9b0b755ab76b124db0bec471ef
Mukesh <mukeshsundarm2531@gmail.com>
2026-02-13 00:37:33 +0530

    Added CORS for GitHub Pages

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.post("/students", (req, res) => {
 
 const PORT=process.env.PORT || 3000;
 app.listen(PORT,() => {
-        console.log('server running on port ${PORT}');
+        console.log(`server running on port ${PORT}`);
 });
