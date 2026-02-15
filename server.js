33c340f00f0d68d8d7579ca079cbdbdbdd978499
Mukesh <mukeshsundarm2531@gmail.com>
2026-02-16 01:02:14 +0530

    Install Student CRUD Project

--- server.js
+++ server.js
@@ -1,33 +1,77 @@
 const express = require("express");
-const cors=require("cors");
+const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();          // Load .env
+connectDB();              // Connect MongoDB
+
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-let students = [];
+const Student = require("./models/Student");
 
-// Home
-app.get("/", (req, res) => {
-    res.send("Student Registration Backend Running 🚀");
-});
-
-// Get all students
-app.get("/students", (req, res) => {
+// GET all students
+app.get("/students", async (req, res) => {
+  try {
+    const students = await Student.find();
     res.json(students);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-// Add new student
-app.post("/students", (req, res) => {
-    const student = req.body;
-    students.push(student);
-    res.json({
-        message: "Student added successfully!",
-        student: student
+// ADD student
+app.post("/students", async (req, res) => {
+  try {
+    const student = new Student({
+      name: req.body.name,
+      course: req.body.course
     });
+
+    const savedStudent = await student.save();
+    res.status(201).json(savedStudent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-const PORT=process.env.PORT || 3000;
-app.listen(PORT,() => {
-        console.log(`server running on port ${PORT}`);
+// UPDATE student
+app.put("/students/:id", async (req, res) => {
+  try {
+    const updatedStudent = await Student.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        course: req.body.course
+      },
+      { new: true }
+    );
+
+    res.json(updatedStudent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// DELETE student
+app.delete("/students/:id", async (req, res) => {
+  try {
+    await Student.findByIdAndDelete(req.params.id);
+    res.json({ message: "Student deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
 });
